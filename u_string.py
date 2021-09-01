f6f637262ec14a0b59b4322f40c568a3e48dc4a7
feiandxs <yiziyint@gmail.com>
2021-09-01 16:06:50 +0800

    added conver str to list

--- u_string.py
+++ u_string.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # ibegyourpardon
+
+
+def convert_str_to_list(original: str, u_seperator: str = ',') -> list:
+    result = original.split(u_seperator)
+    return result
+
+
+
+
