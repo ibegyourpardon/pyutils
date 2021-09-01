c467095aff4e950acbd28e1c6b0ab6036557f4d3
feiandxs <yiziyint@gmail.com>
2021-09-01 15:52:27 +0800

    1

--- rand.py
+++ rand.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # ibegyourpardon
 import random
+import string
 
 
 def random_integer(min_num: int = 0, max_num: int = 100, step: int = 1):
@@ -15,5 +16,11 @@ def random_fload(min_num: int = 0, max_num: int = 100):
     return random.uniform(min_num, max_num)
 
 
-def random_string(num: int = 8):
-    safe_string = 'abcdefghijklmnopqrstuvwxyz'
+def simple_random_string(num: int = 8):
+    # string.printable=string.ascii_letters(大小写）+string.digits(数字)+string.punctuation(标点符号）+string.whitespace(空白符号）
+    x = string.ascii_letters
+    salt = ''
+    for i in range(num):
+        salt += random.choice(x)
+    return salt
+
