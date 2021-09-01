760a021e5b053e09c21ca0fc1e9a3b3d6832f11b
feiandxs <yiziyint@gmail.com>
2021-09-01 16:24:14 +0800

    ?

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ibegyourpardon
+
+from setuptools import setup
+
+setup(
+    name='pyutils',
+    version='0.0.1',
+    author='ibegyourpardon',
+    author_email='ibegyourpardon@sc2l.com',
+    url='https://github.com',
+    description=u'my pyutils ',
+    packages=['pyutils'],
+    install_requires=[],
+)
