4d5eef9e7682fef5fa1e3e77bc315d603411900e
feiandxs <yiziyint@gmail.com>
2021-09-07 11:03:11 +0800

    1

--- pyutils/u_request.py
+++ pyutils/u_request.py
@@ -1,2 +1,18 @@
 import requests
 
+
+class RequestHandler:
+
+    def __init__(self, method, url, payload):
+        self.method = method
+        self.url = url
+        self.payload = payload
+
+    def get(self, url, **kwargs):
+        params = kwargs.get("params")
+        headers = kwargs.get("headers")
+        try:
+            result = requests.get(url=url,params=params, headers=headers)
+            return result
+        except Exception as e:
+            print("post请求错误: %s" % e)
